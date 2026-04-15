54d72a10af01244ffd34d621feefd72676256228
Muhammad Ranju <mdranju23@gmail.com>
2026-04-16 00:58:29 +0600

    feat: initialize express application with middleware, routing, and global error handling

--- src/app.ts
+++ src/app.ts
@@ -34,11 +34,10 @@ app.use('/api/v1', router);
 //live response
 app.get('/', (req: Request, res: Response) => {
   const date = new Date(Date.now());
-  res.send(
-    `<h1 style="text-align:center; color:#173616; font-family:Verdana;">Beep-beep! The server is alive and kicking.</h1>
-    <p style="text-align:center; color:#173616; font-family:Verdana;">${date}</p>
-    `,
-  );
+  res.json({
+    message: 'Beep-beep! The server is alive and kicking.',
+    date,
+  });
 });
 
 //global error handle
